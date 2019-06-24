6ab2676f5c9c2bf501a9455c47126f17d25a091c
Kalhara Perera <adorekasun@gmail.com>
2019-06-24 16:37:47 +0530

    Functionals

--- Main.py
+++ Main.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import random
+import time
+
+# taking the attempts
+attempts = input('Enter the attempts you want [1-6] : ')
+print('Number of Attempts : ' + attempts)
+print('Selecting a Number....')
+time.sleep(1)
+
+# selecting a word
 line = random.choice(open('output-onlinerandomtools.csv').readlines())
-print(line)
+# print(line)
+
+# printing the count
 finalcount = len(line) - 3
 print(finalcount)
+
+# printingstar = line-3
+
+finalfinal = str(finalcount)
+for i in range(finalcount):
+    print('*', end=" ")
+
+print('')
